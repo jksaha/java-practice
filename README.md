ef351a5142f98466f734ca88d52addc189d19d00
Jayanta Saha <jksaha@gmail.com>
2020-04-10 21:59:53 +0100

    feat: added the skeleton and the some hints

--- README.md
+++
@@ -1,2 +0,0 @@
-# java-practice
-This is a project where we will write some java sample examples and understand them.
---
+++ src/com/saha/Main.java
@@ -0,0 +1,25 @@
+package com.saha;
+
+public class Main {
+
+    public static void main(String[] args) {
+	/*
+	Problem:
+	Given two strings implement a method which determines whether the
+	characters in the second string is a substring of the characters
+	 in the first string (check if second string exists as continuous/unbroken
+	 chain of characters the first string).
+	 */
+	//System.out.println("Test");
+        boolean result = isSubstring("go home", "ome");
+        System.out.println("Decision :"+result);
+
+    }
+    //isSubstring("go home", "ome") // true
+    public static boolean isSubstring(String firstString, String secondString){
+        //Write the logic so that you can
+        //determine that the second string is the substring of the first one
+
+        return true;
+    }
+}
